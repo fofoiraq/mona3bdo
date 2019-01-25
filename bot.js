68c94a2561ed8d66723454874ffbd14df1370105
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-25 20:02:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,6 +74,21 @@ client.on('message', async message => {
             ADD_REACTIONS: false
           });
         });
+	      client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith('>ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('**Time Taken:**',msg + " ms 📶 ")
+                        .addField('**WebSocket:**',api + " ms 📶 ")
+         message.channel.send({embed:embed});
+                        }
+                    });
       }catch(e){
         console.log(e.stack);
       }
