5a2746ce0dda3ad18337090ebfbfce04f0b5d6d3
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-01-31 20:13:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ client.on('ready', function(){
 client.on('message', message => {
     if (message.content.startsWith("<@537238168525733898>"))
     
-    message.reply("كسمك");
+    message.reply("+help");
     
       
 
