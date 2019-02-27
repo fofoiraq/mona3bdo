2ae57bf1448fdc456121f45048545f835d3c17c8
discord <43854305+hgfhjtfuy@users.noreply.github.com>
2019-02-27 20:43:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,25 +62,70 @@ client.on('message', message => {
   .catch(console.error);
 }
 });
-const userslist = new Set();
-var bestsupports = ['537147937583529994','518214960958930950'];// ايديات الي يقدرو يضيفو
-client.on('message',async message => {
-  if(!bestsupports.includes(message.auhtor.id)) return;
-  if(message.content.startsWith(prefix + "addB")) {
-    let user = message.mentions.users.first();
-    if(userslist.has(user.id)) return message.reply('**الشخص في البلاك لست من قبل**')
-    userslist.add(user.id);
-    message.channel.send(`Added user: ${user}`);
-  } else {
-    if(message.content.startsWith(prefix + "remB")) {
-    let user = message.mentions.users.first();
-    if(!userslist.has(user.id)) return message.reply('**لا يوجد شخص بـ هذ المنشن**');
-    userslist.delete(user.id);
-    message.channel.send(`Removed user: ${user}`);
-  }
-  }
-});
 
+var p = "#";
+client.on('message', message => {
+         if (message.content === prefix + "time") {
+         if (!message.channel.guild) return message.reply('** This command only for servers **');  
+         var currentTime = new Date(),
+            hours = currentTime.getHours() + 4 ,
+            hours2 = currentTime.getHours() + 3 ,
+            hours3 = currentTime.getHours() + 2 ,
+            hours4 = currentTime.getHours() + 3 ,
+            minutes = currentTime.getMinutes(),
+            seconds = currentTime.getSeconds(),
+            Year = currentTime.getFullYear(),
+            Month = currentTime.getMonth() + 1,
+            Day = currentTime.getDate();
+             var h = hours
+  if(hours > 12) {
+               hours -= 12;
+            } else if(hours == 0) {
+                hours = "12";
+            }  
+             if(hours2 > 12) {
+               hours2 -= 12;
+            } else if(hours2 == 0) {
+                hours2 = "12";
+            
+            }  
+                         if(hours3 > 12) {
+               hours3 -= 12;
+            } else if(hours3 == 0) {
+                hours3 = "12";
+            } 
+            if (minutes < 10) {
+                minutes = '0' + minutes;
+            }
+
+            var suffix = 'صباحاَ';
+            if (hours >= 12) {
+                suffix = 'مساء';
+                hours = hours - 12;
+            }
+            if (hours == 0) {
+                hours = 12;
+            }
+ 
+
+                var Date15= new Discord.RichEmbed()
+                .setThumbnail("https://i.imgur.com/ib3n4Hq.png") 
+                .setTitle( "『التاريخ  والوقت』")
+                .setColor('RANDOM')
+                .setFooter(message.author.username, message.author.avatarURL)
+                .addField('الامارات',
+                "『"+ hours + ":" + minutes +":"+ seconds + "』")
+                 .addField('مكه المكرمه',
+                "『"+ hours2 + ":" + minutes +":"+ seconds  + "』") 
+                .addField('مصر',
+                "『"+ hours3 + ":" + minutes +":"+ seconds  + "』") 
+                
+                .addField('Date',
+                "『"+ Day + "-" + Month + "-" + Year +  "』")
+
+                 message.channel.sendEmbed(Date15);
+        }
+    });
 
 client.on('message', message => {
     if(message.content == ('#profile')) {    
